9754b3dacbb6d58960caf61d66d096d00da1a9c0
rbrtotavio <50200342+rbrtotavio@users.noreply.github.com>
2019-08-23 00:37:29 -0300

    Add files via upload

---
+++ Mapa.py
@@ -0,0 +1,35 @@
+def welsh_powell(grafo):                                                            #Aqui defino a função que aplicará o algoritimo Welsh-Powell
+    pontos = sorted(list(grafo.keys()), key=lambda x: len(grafo[x]), reverse=True)  #Nesta linha organizo cada vértice de acordo com sua quantidade de
+    cor_mapa = {}                                                                   #arestas de forma decrescente, já que o algoritimo precisa que as vertices
+                                                                                    #estejam organizadas de tal forma.
+    for ponto in pontos:                                #Aqui ponho em pratica o algoritimo, primeiramente estabelecendo
+        cor_disp = [True] * len(pontos)                 #O pior caso: Cada vértice precisar de uma cor distinta. E então
+        for vizinho in grafo[ponto]:                    #estabeleco que para cada vizinho haja um índice de preenchimento
+            if vizinho in cor_mapa:                     #de suas cores(de 0 à n, sendo 0 uma primeira cor hipotética).
+                cor = cor_mapa[vizinho]                 #Cada vértice é uma chave em um dicionario e cada indice é um valor.
+                cor_disp[cor] = False                   #cor_disp guarda o valor booleano para verificar se a cor selecionada
+        for cor, disponivel in enumerate(cor_disp):     #para certa aresta está disponível para a mesma ou não.
+            if disponivel:                              #Se disponível, a função será concluida e repetida até o fim das arestas,
+                cor_mapa[ponto] = cor                   #se indisponível um novo índice da ordem de preenchimento será aplicado.
+                break
+
+    return cor_mapa                                     #esta função retorna cada vértice(chave) e seus respectivos índices de cor(valor).
+
+
+if __name__ == "__main__":
+    grafo = {'a':{'b', 'e'},
+             'b':{'a', 'e', 'f', 'g', 'c'},
+             'c':{'b', 'g', 'h', 'd'},
+             'd':{'c', 'h'},
+             'e':{'a', 'b', 'f', 'h'},
+             'f':{'e', 'b', 'g', 'h'},
+             'g':{'f', 'b', 'c', 'h'},
+             'h':{'e', 'f', 'g', 'c', 'd'}}
+
+print(welsh_powell(grafo))
+
+cores = list(welsh_powell(grafo).values())              #Aqui eu tomo os índices atribuidos ao dicionário, e então,
+cores = list(dict.fromkeys(cores))                      #removo as repetições para contar quantas cores foram usadas
+qtd_cores = len(cores)                                  #para colorir o mapa.
+
+print("O Mapa precisa de, no mínimo, {} cores!" .format(qtd_cores))
